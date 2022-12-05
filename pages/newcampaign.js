ab2d8038da3e73c57eafeaee32a2ce77a6c13902
varManWai <laizoke98@gmail.com>
2022-12-05 23:42:16 +0800

    clean setup

---
+++ pages/newcampaign.js
@@ -0,0 +1,7 @@
+export default function show() {
+    return (
+        <div>
+            <h1>this is the new campaign page!!!</h1>
+        </div>
+    )
+};
---
+++ pages/show.js
@@ -0,0 +1,7 @@
+export default function show() {
+    return (
+        <div>
+            <h1>this is the show page!!!</h1>
+        </div>
+    )
+};
