a45efa17ca4670d326972f61959b1d7748ef9565
varManWai <laizoke98@gmail.com>
2022-12-06 14:23:34 +0800

    completed the basic create campaign in the page

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,25 +1,44 @@
 import { useState } from "react";
-import { Form, Button, Input} from "semantic-ui-react";
+import { Form, Button, Input, Message } from "semantic-ui-react";
+
+import factory from "../../ethereum/factory";
+import web3 from "../../ethereum/web3";
 
 export default function CampaignNew() {
-  const [minimumContribution, setMinimumContribution] = useState('')
+  const [minimumContribution, setMinimumContribution] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods.createCampaign(minimumContribution).send({
+        from: accounts[0],
+      });
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+  };
+
   return (
     <div>
-       <h3>Create Campaign</h3>
-        <Form>
-          <Form.Field>
-            <label>Minimum Contribution</label>
-            <Input label="wei" labelPosition="right"
+      <h3>Create Campaign</h3>
+      <Form onSubmit={onSubmit} error={!!errorMessage}>
+        <Form.Field>
+          <label>Minimum Contribution</label>
+          <Input
+            label="wei"
+            labelPosition="right"
             value={minimumContribution}
-            onChange={(event)=>{
+            onChange={(event) => {
               setMinimumContribution(event.target.value);
             }}
-            />
-          </Form.Field>
-          <Button primary>Create!</Button>
-        </Form>
+          />
+        </Form.Field>
+        <Message error header="Oops!" content={errorMessage} />
+        <Button primary>Create!</Button>
+      </Form>
     </div>
   );
 }
