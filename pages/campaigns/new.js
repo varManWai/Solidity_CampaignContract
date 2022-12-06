45729a528e32100085b8e45a512c16fae33f86a3
varManWai <laizoke98@gmail.com>
2022-12-06 14:07:27 +0800

    added the input handler

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import { Form, Button, Input} from "semantic-ui-react";
+
 export default function CampaignNew() {
+  const [minimumContribution, setMinimumContribution] = useState('')
+
+  
   return (
     <div>
-      <h1>New Campaign!</h1>
+       <h3>Create Campaign</h3>
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input label="wei" labelPosition="right"
+            value={minimumContribution}
+            onChange={(event)=>{
+              setMinimumContribution(event.target.value);
+            }}
+            />
+          </Form.Field>
+          <Button primary>Create!</Button>
+        </Form>
     </div>
   );
 }
